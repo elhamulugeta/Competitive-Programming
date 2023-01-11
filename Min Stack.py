f388b0beca27de953cbfd3f3430e751177bfd3d0
elhamulugeta <111562989+elhamulugeta@users.noreply.github.com>
2023-01-11 20:33:29 +0300

    Create Min Stack.py

---
+++ Min Stack.py
@@ -0,0 +1,34 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        return self.stack.append(val)
+        
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        return self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.stack)
+        
